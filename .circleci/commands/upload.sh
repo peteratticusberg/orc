3cf8f03c6231ae22b071278eeb9b378124a6de93
Peter Berg <atticusberg@gmail.com>
2018-01-08 16:12:22 -0500

    Add a script to upload json versions of our markdown files to s3

---
+++ .circleci/commands/upload.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "uploading to s3..."
+
+json_dir="coin_json"
+
+mkdir "$json_dir"
+for markdown_file in lib/*
+do
+  json_file=$(basename $markdown_file | sed -e 's/.md/.json/')
+  bin/jsonify.sh "$markdown_file" > "$json_dir/$json_file"
+done
+aws s3 sync "./$json_dir" s3://coininfo --delete
