e89e5e8cd746d5be1b15d64da4c581a9c104cfe3
Peter Berg <atticusberg@gmail.com>
2018-02-08 09:52:11 -0500

    Update upload.sh

--- bin/support/build.sh
+++ bin/support/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-domain="$1"
+domain="$1" # this is used for hyperlinking content to other content 
 project_root="$(pwd)"
 build_dir="$project_root/build"
 mkdir -p "$build_dir/coins"
@@ -8,12 +8,12 @@ mkdir -p "$build_dir/glossary"
 
 link_dictionary="$(printf "$(bin/support/link_dictionary.sh)")"
 
-function transform() {
+function build() {
   markdown_file="$1"
   parent_dir="$2" # expects coins or glossary
   output_file="$build_dir/$parent_dir/$(basename $markdown_file | sed -e 's/.md/.json/')"
 
-  # You can't read from a file while you're also outputting to it so we have to write to file on a separate line
+  # You can't simulataneously read from and write to a file, hence the use of two separate lines for these transformations
   transformation="$(bin/support/jsonify.rb "$markdown_file")"
   echo "$transformation" > "$output_file"
 
@@ -27,11 +27,11 @@ function transform() {
 for markdown_file in lib/coins/*.md
 do
   echo "Transforming $markdown_file..."
-  transform "$(pwd)/$markdown_file" coins
+  build "$(pwd)/$markdown_file" coins
 done
 
 for markdown_file in lib/glossary/*.md
 do
   echo "Transforming $markdown_file..."
-  transform "$(pwd)/$markdown_file" glossary
+  build "$(pwd)/$markdown_file" glossary
 done
