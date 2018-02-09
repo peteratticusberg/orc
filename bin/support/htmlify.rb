00fdf42b3cd1587ce1d661ba9e7d8ce20c91a04c
Peter Berg <atticusberg@gmail.com>
2018-02-09 09:00:03 -0500

    Strip enclosing p tags from html

--- bin/support/htmlify.rb
+++ bin/support/htmlify.rb
@@ -6,7 +6,10 @@ original_file_values = JSON.parse(File.read(ARGV[0]))
 transformed_file_values = {}
 
 original_file_values.each do |header, content|
-  transformed_file_values[header] = Kramdown::Document.new(content).to_html
+  html = Kramdown::Document.new(content).to_html
+  html = html.strip
+  html = html.sub(/^<p>/, "").sub(/<\/p>$/, "") # strip enclosing ptags
+  transformed_file_values[header] = html
 end
 
 puts transformed_file_values.to_json
