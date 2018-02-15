7e2e1c153c317ed925164befb5f88f08b49ca6e7
Peter Berg <atticusberg@gmail.com>
2018-02-15 09:02:29 -0500

    Don’t strip p tags for glossary content

--- bin/support/htmlify.rb
+++ bin/support/htmlify.rb
@@ -8,7 +8,9 @@ transformed_file_values = {}
 original_file_values.each do |header, content|
   html = Kramdown::Document.new(content).to_html
   html = html.strip
-  html = html.sub(/^<p>/, "").sub(/<\/p>$/, "") # strip enclosing ptags
+  unless header == "content"
+    html = html.sub(/^<p>/, "").sub(/<\/p>$/, "") # strip enclosing ptags
+  end
   transformed_file_values[header] = html
 end
 
