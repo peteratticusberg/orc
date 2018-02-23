b6d22ead48612a9d7a9aa54e8a7a8f96f2c36d39
Peter Berg <atticusberg@gmail.com>
2018-02-23 18:00:41 -0500

    Support sequencing of hyperlinkable terms such that substrings like “Dapp” only get linked after superstrings like “Dapp Platform” do

--- bin/support/link_dictionary.rb
+++ bin/support/link_dictionary.rb
@@ -23,13 +23,29 @@ files.each do |file|
     section_content = sections[section]
     if section_content
       section_content.split(",").each do |link_key|
-        link_key = link_key.strip
+        link_key = link_key.strip.downcase
         link_dictionary[link_key] = link_value
         pluralized_link_key = "#{link_key}s" # poor yet comprehensible attempt at pluralizing for now
         link_dictionary[pluralized_link_key] = link_value
       end
     end
   end
+end
+
+substring_keys = [] # keys that are a substring of another key. E.g., the key "Dapp" is a substring of the key "Dapp Platform"
+link_dictionary.each do |link_key_a|
+  link_dictionary.each do |link_key_b|
+    if link_key_b.include?(link_key_a)
+      substring_keys << link_key_a
+    end
+  end
+end
+
+# Set these substring keys as the last keys in the link dictionary so that keys like "Dapp Platform" get hyperlinked before keys like "Dapp" do.
+substring_keys.each do |key|
+  value = link_dictionary[key]
+  link_dictionary.delete[key]
+  link_dictionary[key] = value
 end
 
 puts link_dictionary.to_json
